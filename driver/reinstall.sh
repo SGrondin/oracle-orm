5d855481d34de5289b71ad9da9785e6129291016
SGrondin <github@simongrondin.name>
2014-05-14 16:00:39 -0400

    0.0.6 Fix install scripts

--- driver/reinstall.sh
+++ driver/reinstall.sh
@@ -1,27 +1,26 @@
 #! /usr/bin/env bash
 
 module=$(pwd)
-pushd $module/../.. > /dev/null
+pushd $module/../..
 project=$(pwd)
-popd > /dev/null
+popd
 
-if [[ $module/driver/installed ]]; then
+if [[ -f $module/driver/installed ]]; then
+	echo 'exiting'
 	exit
 fi
 
 source $module/driver/exports.sh
 
-pushd $OCI_LIB_DIR > /dev/null
+pushd $OCI_LIB_DIR
 unlink libocci.so
 unlink libclntsh.so
 ln -s libocci.so.12.1 libocci.so
 ln -s libclntsh.so.12.1 libclntsh.so
-popd > /dev/null
+popd
 
 echo $OCI_HOME/ | sudo tee /etc/ld.so.conf.d/oracle_instant_client.conf
 sudo ldconfig
 
-pushd $module > /dev/null
 touch driver/installed
-npm install --dev
-popd > /dev/null
+npm install
